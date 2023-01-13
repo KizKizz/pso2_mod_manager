63da15105c16d9df6f3a4871af14da4d67fb9aee
KizKizz <101075148+KizKizz@users.noreply.github.com>
2023-01-12 23:28:03 -0800

    bni

--- lib/mod_add_handler.dart
+++ lib/mod_add_handler.dart
@@ -4,6 +4,7 @@ import 'package:cross_file/cross_file.dart';
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter/material.dart';
 import 'package:archive/archive_io.dart';
+// ignore: depend_on_referenced_packages
 import 'package:collection/collection.dart';
 
 // ignore: depend_on_referenced_packages
@@ -92,7 +93,7 @@ void modAddHandler(context) {
                                       decoration: BoxDecoration(
                                         borderRadius: BorderRadius.circular(3),
                                         border: Border.all(color: Theme.of(context).hintColor),
-                                        color: _newModDragging ? Colors.blue.withOpacity(0.4) : Colors.black26,
+                                        color: _newModDragging ? Colors.blue.withOpacity(0.4) : Colors.black26.withAlpha(20),
                                       ),
                                       height: constraints.maxHeight - 33,
                                       width: constraints.maxWidth * 0.3,
@@ -114,7 +115,7 @@ void modAddHandler(context) {
                                                           itemCount: _newModDragDropList.length,
                                                           itemBuilder: (BuildContext context, int index) {
                                                             return ListTile(
-                                                              dense: true,
+                                                              //dense: true,
                                                               // leading: const Icon(
                                                               //     Icons.list),
                                                               trailing: SizedBox(
@@ -176,11 +177,15 @@ void modAddHandler(context) {
                                                   ? (() async {
                                                       for (var files in _newModDragDropList) {
                                                         if (p.extension(files.path) == '.zip') {
-                                                          await unzipPack(files.path, files.name);
-                                                          modsToAddList.addAll(await sortFile(files.name));
+                                                          await unzipPack(files.path, files.name.split('.').first);
+                                                          modsToAddList.addAll(await sortFile(files.name.split('.').first));
                                                         } else {
                                                           modsToAddList.add(XFile(files.path));
                                                         }
+                                                      }
+
+                                                      for (var element in modsToAddList) {
+                                                        print(element.path);
                                                       }
 
                                                       //clear lists
@@ -264,6 +269,7 @@ void modAddHandler(context) {
                                           );
                                         } else {
                                           sortedModsList = snapshot.data;
+                                          //print(sortedModsList.length);
                                           return SingleChildScrollView(
                                               controller: AdjustableScrollController(80),
                                               child: ListView.builder(
@@ -281,24 +287,29 @@ void modAddHandler(context) {
                                                       child: ExpansionTile(
                                                         initiallyExpanded: true,
                                                         title: curActiveLang == 'JP'
-                                                            ? Text('${sortedModsList[index].first} > ${sortedModsList[index][1]}')
-                                                            : Text('${sortedModsList[index].first} > ${sortedModsList[index][2]}'),
+                                                            ? Text('${sortedModsList[index].first} > ${sortedModsList[index][1]}', style: const TextStyle(fontWeight: FontWeight.w500,))
+                                                            : Text('${sortedModsList[index].first} > ${sortedModsList[index][2]}', style: const TextStyle(fontWeight: FontWeight.w500,)),
                                                         textColor: MyApp.themeNotifier.value == ThemeMode.light ? Theme.of(context).primaryColor : Theme.of(context).iconTheme.color,
                                                         iconColor: MyApp.themeNotifier.value == ThemeMode.light ? Theme.of(context).primaryColor : Theme.of(context).iconTheme.color,
                                                         collapsedTextColor: MyApp.themeNotifier.value == ThemeMode.light ? Theme.of(context).primaryColor : Theme.of(context).iconTheme.color,
                                                         children: [
-                                                          ExpansionTile(
-                                                            initiallyExpanded: true,
-                                                            title: Text(sortedModsList[index][3]),
-                                                            children: [
-                                                              for (int i = 0; i < modsToAddList.length; i++)
-                                                                if (sortedModsList[index].last.contains(modsToAddList[i].name) && modsToAddList[i].path.contains(sortedModsList[index][3]))
-                                                                  ListTile(
-                                                                    title: Text(modsToAddList[i].name),
-                                                                    dense: true,
-                                                                  )
-                                                            ],
-                                                          )
+                                                          for (int ex = 0; ex < sortedModsList[index][3].split('|').length; ex++)
+                                                            ExpansionTile(
+                                                              initiallyExpanded: false,
+                                                              textColor: MyApp.themeNotifier.value == ThemeMode.light ? Theme.of(context).primaryColor : Theme.of(context).iconTheme.color,
+                                                        iconColor: MyApp.themeNotifier.value == ThemeMode.light ? Theme.of(context).primaryColor : Theme.of(context).iconTheme.color,
+                                                        collapsedTextColor: MyApp.themeNotifier.value == ThemeMode.light ? Theme.of(context).primaryColor : Theme.of(context).iconTheme.color,
+                                                              title: Text(sortedModsList[index][3].split('|')[ex]),
+                                                              children: [
+                                                                for (int i = 0; i < modsToAddList.length; i++)
+                                                                  if (sortedModsList[index].last.split('|').indexWhere((element) => element == modsToAddList[i].path) != -1 &&
+                                                                      modsToAddList[i].path.contains(sortedModsList[index][3].split('|')[ex]))
+                                                                    ListTile(
+                                                                      title: Text(modsToAddList[i].name),
+                                                                      //dense: true,
+                                                                    )
+                                                              ],
+                                                            )
                                                         ],
                                                       ),
                                                     );
@@ -343,7 +354,7 @@ Future<List<XFile>> sortFile(String fileName) async {
   List<XFile> filesList = [];
   String tempDirPath = '${Directory.current.path}${s}temp$s';
   for (var file in Directory('$tempDirPath$fileName$s').listSync(recursive: true)) {
-    if (p.extension(file.path) == '') {
+    if (p.extension(file.path) == '' && !Directory(file.path).existsSync()) {
       XFile newFile = XFile(file.path);
       filesList.add(newFile);
     }
@@ -355,13 +366,59 @@ Future<List<XFile>> sortFile(String fileName) async {
 Future<List<List<String>>> fetchItemName(List<XFile> inputFiles) async {
   Function deepEq = const DeepCollectionEquality().equals;
   List<List<String>> fileList = [];
+  List<XFile> extraFiles = [];
+
   for (var inputFile in inputFiles) {
-    if (fileList.isEmpty) {
-      fileList.add(await findItemInCsv(inputFile));
+    if (p.extension(inputFile.path) != '') {
+      extraFiles.add(inputFile);
     } else {
-      var list = await findItemInCsv(inputFile);
-      if (fileList.indexWhere((element) => deepEq(element, list)) == -1) {
-        fileList.add(list);
+      if (fileList.isEmpty) {
+        fileList.add(await findItemInCsv(inputFile));
+      } else {
+        List<String> curFilePath = inputFile.path.split(s);
+        curFilePath.removeRange(inputFile.path.split(s).length - 1, inputFile.path.split(s).length);
+        String curSubFolder = curFilePath.removeLast();
+        String curDirPath = curFilePath.join(s);
+        bool foundInList = false;
+        for (var line in fileList) {
+          if (line[4] == curDirPath) {
+            foundInList = true;
+            if (line[3].split('|').indexWhere((element) => element == curSubFolder) == -1) {
+              line[3] += '|$curSubFolder';
+            }
+            if (line.last.split('|').indexWhere((element) => element == inputFile.path) == -1) {
+              line.last += '|${inputFile.path}';
+            }
+            break;
+          }
+          //print(line);
+        }
+        if (!foundInList) {
+          var list = await findItemInCsv(inputFile);
+          if (fileList.indexWhere((element) => deepEq(element, list)) == -1) {
+            fileList.add(list);
+          }
+        }
+      }
+    }
+    if (extraFiles.isNotEmpty) {
+      for (var extraFile in extraFiles) {
+        List<String> curFilePath = extraFile.path.split(s);
+        curFilePath.removeRange(extraFile.path.split(s).length - 1, extraFile.path.split(s).length);
+        String curSubFolder = curFilePath.removeLast();
+        String curDirPath = curFilePath.join(s);
+        for (var line in fileList) {
+          if (line[4] == curDirPath) {
+            if (line[3].split('|').indexWhere((element) => element == curSubFolder) == -1) {
+              line[3] += '|$curSubFolder';
+            }
+            if (line.last.split('|').indexWhere((element) => element == inputFile.path) == -1) {
+              line.last += '|${extraFile.path}';
+            }
+            break;
+          }
+          //print(line);
+        }
       }
     }
   }
@@ -381,7 +438,7 @@ Future<List<String>> findItemInCsv(XFile inputFile) async {
           if (index != -1) {
             filePathSplit.removeRange(index, filePathSplit.length);
             newFilePath = filePathSplit.join(s);
-            print(newFilePath);
+            //print(newFilePath);
             break;
           }
         }
@@ -389,12 +446,16 @@ Future<List<String>> findItemInCsv(XFile inputFile) async {
           filePathSplit.removeAt(filePathSplit.length - 1);
           newFilePath = filePathSplit.join(s);
         }
+        List<String> curFilePath = inputFile.path.split(s);
+        curFilePath.removeRange(inputFile.path.split(s).length - 2, inputFile.path.split(s).length);
+        String curDirPath = curFilePath.join(s);
+        //[0 Category, 1 JP name, 2 EN name, 3 Parent Folder, 4 path, 5 new path, 6 sheets, 7 files]
         if (_emoteCsv.indexWhere((element) => file.first == element) != -1) {
-          return (['Emotes', lineSplit[1], lineSplit[2], newFilePath.split(s).last.split('.').first, newFilePath, line]);
+          return (['Emotes', lineSplit[1], lineSplit[2], newFilePath.split(s).last, curDirPath, newFilePath, line, inputFile.path]);
         } else if (_basewearCsv.indexWhere((element) => element == file.first) != -1) {
-          return (['Basewears', lineSplit[0], lineSplit[1], newFilePath.split(s).last.split('.').first, newFilePath, line]);
+          return (['Basewears', lineSplit[0], lineSplit[1], newFilePath.split(s).last, curDirPath, newFilePath, line, inputFile.path]);
         } else {
-          return ([file.first, lineSplit[0], lineSplit[1], newFilePath.split(s).last.split('.').first, newFilePath, line]);
+          return ([file.first, lineSplit[0], lineSplit[1], newFilePath.split(s).last, curDirPath, newFilePath, line, inputFile.path]);
         }
       }
     }
