134a706929fd549a413d26f196a7b7592e3ea0c2
KizKizz <xkizkizz@gmail.com>
2023-04-21 17:45:40 -0700

    prep

--- lib/functions/language_loader.dart
+++ lib/functions/language_loader.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'dart:io';
 
@@ -14,8 +13,7 @@ Uri modManLanguageSettingsJsonPath = Uri();
 TranslationText? curLangText;
 List<TranslationLanguage> languageList = [];
 
-
-TranslationText? UITextLoader() {
+Future<TranslationText> uiTextLoader() async {
   const JsonEncoder encoder = JsonEncoder.withIndent('  ');
   modManLanguageDirPath = Uri.directory('${Directory.current.path}\\Language');
   Directory(modManLanguageDirPath.toFilePath()).createSync();
@@ -28,144 +26,173 @@ TranslationText? UITextLoader() {
     languageList.add(TranslationLanguage('EN', enUITextJsonPath, true));
     TranslationText defaultENLanguage = defaultUILangLoader();
     //Write translation to json
-  File(enUITextJsonPath.toFilePath()).writeAsStringSync(encoder.convert(defaultENLanguage));
-  //Add to language dropdown
-  //langDropDownList.add(newLangTextController.text.toUpperCase());
-  //Write to settings json
-  languageList.map((lang) => lang.toJson()).toList();
-  File(modManLanguageSettingsJsonPath.toFilePath()).writeAsStringSync(encoder.convert(languageList));
-  langDropDownList.add('EN');
+    File(enUITextJsonPath.toFilePath()).writeAsStringSync(encoder.convert(defaultENLanguage));
+    //Add to language dropdown
+    //langDropDownList.add(newLangTextController.text.toUpperCase());
+    //Write to settings json
+    languageList.map((lang) => lang.toJson()).toList();
+    File(modManLanguageSettingsJsonPath.toFilePath()).writeAsStringSync(encoder.convert(languageList));
+    langDropDownList.add('EN');
   } else {
     //Load local json
-  var jsonData = jsonDecode(File(modManLanguageSettingsJsonPath.toFilePath()).readAsStringSync());
-  var languagesFromJson = [];
-  for (var lang in jsonData) {
-    languagesFromJson.add(TranslationLanguage.fromJson(lang));
+    var jsonData = jsonDecode(File(modManLanguageSettingsJsonPath.toFilePath()).readAsStringSync());
+    //List<TranslationLanguage> languagesFromJson = [];
+    for (var lang in jsonData) {
+      languageList.add(TranslationLanguage.fromJson(lang));
+    }
+
+    for (var lang in languageList) {
+      if (!File(lang.langFilePath.path).existsSync()) {
+        //Uri enUITextJsonPath = Uri.file('$modManLanguageDirPath${lang.langInitial}.json');
+        File(lang.langFilePath.path).createSync();
+        TranslationText defaultENLanguage = defaultUILangLoader();
+        File(lang.langFilePath.path).writeAsStringSync(encoder.convert(defaultENLanguage));
+      }
+    }
   }
 
+  //TranslationText returnLanguage;
+  int selectedIndex = languageList.indexWhere((element) => element.selected);
+  if (selectedIndex != -1) {
+    langDropDownList.add(languageList[selectedIndex].langInitial);
+    langDropDownSelected = languageList[selectedIndex].langInitial;
+    var jsonData = jsonDecode(File(languageList[selectedIndex].langFilePath.path).readAsStringSync());
+
+    //returnLanguage = TranslationText.fromJson(jsonData);
+    return TranslationText.fromJson(jsonData);
+  } else {
+    int enIndex = languageList.indexWhere((element) => element.langInitial == 'EN');
+    languageList[enIndex].selected = true;
+    langDropDownList.add(languageList[enIndex].langInitial);
+    langDropDownSelected = languageList[enIndex].langInitial;
+    var jsonData = jsonDecode(File(languageList[enIndex].langFilePath.toFilePath()).readAsStringSync());
+
+    //returnLanguage = TranslationText.fromJson(jsonData);
+    return TranslationText.fromJson(jsonData);
   }
 
+  //return returnLanguage;
 }
 
-Future<void> languagePackCheck() async {
-    curLanguageDirPath = '${Directory.current.path}${s}Language';
-    langSettingsPath = '${Directory.current.path}${s}Language${s}LanguageSettings.json';
+// Future<void> languagePackCheck() async {
+//     curLanguageDirPath = '${Directory.current.path}${s}Language';
+//     langSettingsPath = '${Directory.current.path}${s}Language${s}LanguageSettings.json';
 
-    if (!File(langSettingsPath).existsSync()) {
-      await File(langSettingsPath).create(recursive: true);
-      TranslationLanguage newENLang = TranslationLanguage('EN', '$curLanguageDirPath${s}EN.json', true);
-      await File('$curLanguageDirPath${s}EN.json').create(recursive: true);
-      TranslationText newEN = defaultUILangLoader();
-      //Json Write
-      [newEN].map((translationText) => translationText.toJson()).toList();
-      File('$curLanguageDirPath${s}EN.json').writeAsStringSync(json.encode([newEN]));
-      langList.add(newENLang);
-      langDropDownList.add(newLangTextController.text.toUpperCase());
-      //Json Write
-      langList.map((translation) => translation.toJson()).toList();
-      File(langSettingsPath).writeAsStringSync(json.encode(langList));
-      langList = await translationLoader();
-      for (var lang in langList) {
-        langDropDownList.add(lang.langInitial);
-        if (lang.langFilePath != '$curLanguageDirPath$s${lang.langInitial}.json') {
-          lang.langFilePath = '$curLanguageDirPath$s${lang.langInitial}.json';
-          //Json Write
-          langList.map((translation) => translation.toJson()).toList();
-          File(langSettingsPath).writeAsStringSync(json.encode(langList));
-        }
-        if (lang.selected) {
-          langDropDownSelected = lang.langInitial;
-          curSelectedLangPath = '$curLanguageDirPath$s${lang.langInitial}.json';
-        }
-      }
-    } else {
-      List<TranslationLanguage> tempLangList = await translationLoader();
-      for (var lang in tempLangList) {
-        if (!File(lang.langFilePath).existsSync()) {
-          if (lang.selected) {
-            if (lang.langInitial != 'EN') {
-              tempLangList.singleWhere((element) => element.langInitial == 'EN').selected = true;
-            } else {
-              await File('$curLanguageDirPath${s}EN.json').create(recursive: true);
-              TranslationText newEN = defaultUILangLoader();
-              //Json Write
-              [newEN].map((translationText) => translationText.toJson()).toList();
-              File('$curLanguageDirPath${s}EN.json').writeAsStringSync(json.encode([newEN]));
-            }
-          }
-        } else {
-          if (lang.langInitial == 'EN') {
-            TranslationText newEN = defaultUILangLoader();
-            //Json Write
-            [newEN].map((translationText) => translationText.toJson()).toList();
-            File(lang.langFilePath).writeAsStringSync(json.encode([newEN]));
-          } else {
-            String curLangString = File('$curLanguageDirPath${s}EN.json').readAsStringSync();
-            curLangString = curLangString.replaceRange(0, 2, '');
-            curLangString = curLangString.replaceRange(curLangString.length - 2, null, '');
-            List<String> newTranslationItems = curLangString.split('",');
-            String tempTranslationFromFile = File(lang.langFilePath).readAsStringSync();
-            tempTranslationFromFile = tempTranslationFromFile.replaceRange(0, 2, '');
-            tempTranslationFromFile = tempTranslationFromFile.replaceRange(tempTranslationFromFile.length - 2, null, '');
-            List<String> curTranslationItems = tempTranslationFromFile.split('",');
-            curTranslationItems.last = curTranslationItems.last.replaceRange(curTranslationItems.last.length - 1, null, '');
-            String curLastItem = curTranslationItems.last;
+//     if (!File(langSettingsPath).existsSync()) {
+//       await File(langSettingsPath).create(recursive: true);
+//       TranslationLanguage newENLang = TranslationLanguage('EN', '$curLanguageDirPath${s}EN.json', true);
+//       await File('$curLanguageDirPath${s}EN.json').create(recursive: true);
+//       TranslationText newEN = defaultUILangLoader();
+//       //Json Write
+//       [newEN].map((translationText) => translationText.toJson()).toList();
+//       File('$curLanguageDirPath${s}EN.json').writeAsStringSync(json.encode([newEN]));
+//       langList.add(newENLang);
+//       langDropDownList.add(newLangTextController.text.toUpperCase());
+//       //Json Write
+//       langList.map((translation) => translation.toJson()).toList();
+//       File(langSettingsPath).writeAsStringSync(json.encode(langList));
+//       langList = await translationLoader();
+//       for (var lang in langList) {
+//         langDropDownList.add(lang.langInitial);
+//         if (lang.langFilePath != '$curLanguageDirPath$s${lang.langInitial}.json') {
+//           lang.langFilePath = '$curLanguageDirPath$s${lang.langInitial}.json';
+//           //Json Write
+//           langList.map((translation) => translation.toJson()).toList();
+//           File(langSettingsPath).writeAsStringSync(json.encode(langList));
+//         }
+//         if (lang.selected) {
+//           langDropDownSelected = lang.langInitial;
+//           curSelectedLangPath = '$curLanguageDirPath$s${lang.langInitial}.json';
+//         }
+//       }
+//     } else {
+//       List<TranslationLanguage> tempLangList = await translationLoader();
+//       for (var lang in tempLangList) {
+//         if (!File(lang.langFilePath).existsSync()) {
+//           if (lang.selected) {
+//             if (lang.langInitial != 'EN') {
+//               tempLangList.singleWhere((element) => element.langInitial == 'EN').selected = true;
+//             } else {
+//               await File('$curLanguageDirPath${s}EN.json').create(recursive: true);
+//               TranslationText newEN = defaultUILangLoader();
+//               //Json Write
+//               [newEN].map((translationText) => translationText.toJson()).toList();
+//               File('$curLanguageDirPath${s}EN.json').writeAsStringSync(json.encode([newEN]));
+//             }
+//           }
+//         } else {
+//           if (lang.langInitial == 'EN') {
+//             TranslationText newEN = defaultUILangLoader();
+//             //Json Write
+//             [newEN].map((translationText) => translationText.toJson()).toList();
+//             File(lang.langFilePath).writeAsStringSync(json.encode([newEN]));
+//           } else {
+//             String curLangString = File('$curLanguageDirPath${s}EN.json').readAsStringSync();
+//             curLangString = curLangString.replaceRange(0, 2, '');
+//             curLangString = curLangString.replaceRange(curLangString.length - 2, null, '');
+//             List<String> newTranslationItems = curLangString.split('",');
+//             String tempTranslationFromFile = File(lang.langFilePath).readAsStringSync();
+//             tempTranslationFromFile = tempTranslationFromFile.replaceRange(0, 2, '');
+//             tempTranslationFromFile = tempTranslationFromFile.replaceRange(tempTranslationFromFile.length - 2, null, '');
+//             List<String> curTranslationItems = tempTranslationFromFile.split('",');
+//             curTranslationItems.last = curTranslationItems.last.replaceRange(curTranslationItems.last.length - 1, null, '');
+//             String curLastItem = curTranslationItems.last;
 
-            if (newTranslationItems.length != curTranslationItems.length) {
-              for (var item in newTranslationItems) {
-                if (curTranslationItems.indexWhere((element) => element.substring(0, element.indexOf(':')) == item.substring(0, item.indexOf(':'))) == -1) {
-                  curTranslationItems.insert(newTranslationItems.indexOf(item), item);
-                }
-              }
-              String finalTranslation = curTranslationItems.join('",');
-              finalTranslation = finalTranslation.padLeft(finalTranslation.length + 1, '[{');
-              if (curLastItem == curTranslationItems.last) {
-                finalTranslation = finalTranslation.padRight(finalTranslation.length + 1, '"}]');
-              } else {
-                finalTranslation = finalTranslation.padRight(finalTranslation.length + 1, '}]');
-              }
-              File(lang.langFilePath).writeAsStringSync(finalTranslation);
-            }
-          }
-        }
-      }
-    }
-  }
+//             if (newTranslationItems.length != curTranslationItems.length) {
+//               for (var item in newTranslationItems) {
+//                 if (curTranslationItems.indexWhere((element) => element.substring(0, element.indexOf(':')) == item.substring(0, item.indexOf(':'))) == -1) {
+//                   curTranslationItems.insert(newTranslationItems.indexOf(item), item);
+//                 }
+//               }
+//               String finalTranslation = curTranslationItems.join('",');
+//               finalTranslation = finalTranslation.padLeft(finalTranslation.length + 1, '[{');
+//               if (curLastItem == curTranslationItems.last) {
+//                 finalTranslation = finalTranslation.padRight(finalTranslation.length + 1, '"}]');
+//               } else {
+//                 finalTranslation = finalTranslation.padRight(finalTranslation.length + 1, '}]');
+//               }
+//               File(lang.langFilePath).writeAsStringSync(finalTranslation);
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
 
-Future<TranslationText> getUITextTranslation() async {
-    if (languageList.isEmpty) {
-      var jsonData = jsonDecode(File(modsListJsonPath.toFilePath()).readAsStringSync());
-  var jsonCategories = [];
-  for (var cate in jsonData) {
-    jsonCategories.add(Category.fromJson(cate));
-  }
-      for (var lang in langList) {
-        langDropDownList.add(lang.langInitial);
-        if (lang.langFilePath != '$curLanguageDirPath$s${lang.langInitial}.json') {
-          lang.langFilePath = '$curLanguageDirPath$s${lang.langInitial}.json';
-          //Json Write
-          langList.map((translation) => translation.toJson()).toList();
-          File(langSettingsPath).writeAsStringSync(json.encode(langList));
-        }
-        if (lang.selected) {
-          langDropDownSelected = lang.langInitial;
-          curSelectedLangPath = '$curLanguageDirPath$s${lang.langInitial}.json';
-          curActiveLang = lang.langInitial;
-        }
-      }
-    }
+// Future<TranslationText> getUITextTranslation() async {
+//     if (languageList.isEmpty) {
+//       var jsonData = jsonDecode(File(modsListJsonPath.toFilePath()).readAsStringSync());
+//   var jsonCategories = [];
+//   for (var cate in jsonData) {
+//     jsonCategories.add(Category.fromJson(cate));
+//   }
+//       for (var lang in langList) {
+//         langDropDownList.add(lang.langInitial);
+//         if (lang.langFilePath != '$curLanguageDirPath$s${lang.langInitial}.json') {
+//           lang.langFilePath = '$curLanguageDirPath$s${lang.langInitial}.json';
+//           //Json Write
+//           langList.map((translation) => translation.toJson()).toList();
+//           File(langSettingsPath).writeAsStringSync(json.encode(langList));
+//         }
+//         if (lang.selected) {
+//           langDropDownSelected = lang.langInitial;
+//           curSelectedLangPath = '$curLanguageDirPath$s${lang.langInitial}.json';
+//           curActiveLang = lang.langInitial;
+//         }
+//       }
+//     }
 
-    if (curLangText == null) {
-      curLangText = TranslationText.fromJson(jsonDecode(File(modsListJsonPath.toFilePath()).readAsStringSync()))
-      convertLangTextData(jsonDecode(File(curSelectedLangPath).readAsStringSync()));
-      //await Future.delayed(const Duration(milliseconds: 500));
-      setState(() {});
-    }
+//     if (curLangText == null) {
+//       curLangText = TranslationText.fromJson(jsonDecode(File(modsListJsonPath.toFilePath()).readAsStringSync()))
+//       convertLangTextData(jsonDecode(File(curSelectedLangPath).readAsStringSync()));
+//       //await Future.delayed(const Duration(milliseconds: 500));
+//       setState(() {});
+//     }
 
-    topBtnMenuItems = [curLangText!.modsFolderBtnText, curLangText!.backupFolderBtnText, curLangText!.deletedItemsBtnText];
-  }
+//     topBtnMenuItems = [curLangText!.modsFolderBtnText, curLangText!.backupFolderBtnText, curLangText!.deletedItemsBtnText];
+//   }
 
-  //Language Loader
+//Language Loader
 Future<List<TranslationLanguage>> translationLoader() async {
   List<TranslationLanguage> langList = [];
   void convertData(var jsonResponse) {
@@ -185,4 +212,3 @@ Future<List<TranslationLanguage>> translationLoader() async {
 
   return langList;
 }
-
--- lib/pages/ui_language_loading_page.dart
+++ lib/pages/ui_language_loading_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pso2_mod_manager/functions/language_loader.dart';
+import 'package:pso2_mod_manager/pages/home_page.dart';
 
 class UILanguageLoadingPage extends StatefulWidget {
   const UILanguageLoadingPage({Key? key}) : super(key: key);
@@ -10,6 +12,66 @@ class UILanguageLoadingPage extends StatefulWidget {
 class _UILanguageLoadingPageState extends State<UILanguageLoadingPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.amber,);
+    return FutureBuilder(
+        future: uiTextLoader(),
+        builder: (
+          BuildContext context,
+          AsyncSnapshot snapshot,
+        ) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  'Loading Data',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CircularProgressIndicator(),
+              ],
+            );
+          } else {
+            if (snapshot.hasError) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '',
+                    style: TextStyle(color: Theme.of(context).textTheme.bodyLarge?.color, fontSize: 20),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(snapshot.error.toString(), softWrap: true, style: TextStyle(color: Theme.of(context).textTheme.bodyLarge?.color, fontSize: 15)),
+                  )
+                ],
+              );
+            } else if (!snapshot.hasData) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Loading Data',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CircularProgressIndicator(),
+                ],
+              );
+            } else {
+              curLangText = snapshot.data;
+              return const HomePage();
+            }
+          }
+        });
   }
 }
