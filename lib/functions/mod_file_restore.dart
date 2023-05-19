a614cc8fd3099600d1a2e82d13570f4f7e0057bc
KizKizz <xkizkizz@gmail.com>
2023-05-19 16:25:18 -0700

    prep

--- lib/functions/mod_file_restore.dart
+++ lib/functions/mod_file_restore.dart
@@ -2,16 +2,16 @@ import 'package:pso2_mod_manager/classes/category_type_class.dart';
 import 'package:pso2_mod_manager/classes/mod_file_class.dart';
 import 'package:pso2_mod_manager/functions/unapply_mods.dart';
 
-void modFileRestore(List<CategoryType> moddedList, ModFile modFile) {
+Future<void> modFileRestore(List<CategoryType> moddedList, ModFile modFile) async {
   for (var cateType in moddedList) {
     for (var cate in cateType.categories) {
       for (var item in cate.items) {
         for (var mod in item.mods) {
           for (var submod in mod.submods) {
             int modFileIndex = submod.modFiles.indexWhere((element) => element.modFileName == modFile.modFileName);
-            if (modFileIndex != -1) {
+            if (modFileIndex != -1 && submod.modFiles[modFileIndex] != modFile) {
               //submod.modFiles[modFileIndex].applyStatus = false;
-              modFileUnapply(submod.modFiles[modFileIndex]);
+              submod.modFiles[modFileIndex] = await modFileUnapply(submod.modFiles[modFileIndex]);
             }
             if (submod.modFiles.indexWhere((element) => element.applyStatus == true) == -1) {
               submod.applyStatus = false;
