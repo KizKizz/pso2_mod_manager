b0489b9077330b092d8b57dcfc23c46679c621a0
KizKizz <xkizkizz@gmail.com>
2024-08-09 01:34:33 -0700

    prep

--- lib/classes/mod_set_class.dart
+++ lib/classes/mod_set_class.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:pso2_mod_manager/classes/item_class.dart';
 
 part 'mod_set_class.g.dart';
 
 @JsonSerializable()
-class ModSet {
+class ModSet with ChangeNotifier {
   ModSet(this.setName, this.position, this.visible, this.expanded, this.addedDate, this.setItems);
   String setName;
   int position;
@@ -12,6 +13,15 @@ class ModSet {
   bool expanded;
   DateTime addedDate;
   List<Item> setItems;
+
+  void addItem(Item item) {
+    setItems.add(item);
+    notifyListeners();
+  }
+
+  void refresh() {
+    notifyListeners();
+  }
 
   factory ModSet.fromJson(Map<String, dynamic> json) => _$ModSetFromJson(json);
   Map<String, dynamic> toJson() => _$ModSetToJson(this);
