706402bd53b995f7bd856cde3d3ae0210a80586a
KizKizz <xkizkizz@gmail.com>
2025-02-10 15:50:28 -0800

    prep

--- lib/v3_functions/original_ice_download.dart
+++ lib/v3_functions/original_ice_download.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:background_downloader/background_downloader.dart';
 import 'package:pso2_mod_manager/app_localization/app_text.dart';
 import 'package:pso2_mod_manager/global_vars.dart';
-import 'package:pso2_mod_manager/item_aqm_inject/aqm_inject_functions.dart';
 import 'package:signals/signals_flutter.dart';
 import 'package:path/path.dart' as p;
 
@@ -19,15 +18,15 @@ Future<File> originalIceDownload(String networkFilePath, String saveDirLocation,
           updates: Updates.statusAndProgress,
           allowPause: false);
 
-      final result = await FileDownloader().download(task,
-          onProgress: (progress) => status.value = '${appText.dText(appText.downloadingFileName, p.basenameWithoutExtension(networkFilePath))} [ ${(progress * 100).round()}% ]');
+      final result = await FileDownloader()
+          .download(task, onProgress: (progress) => status.value = '${appText.dText(appText.downloadingFileName, p.basenameWithoutExtension(networkFilePath))} [ ${(progress * 100).round()}% ]');
 
       switch (result.status) {
         case TaskStatus.complete:
-          modAqmInjectingStatus.value = appText.fileDownloadSuccessful;
+          status.value = appText.fileDownloadSuccessful;
           return File(saveDirLocation + p.separator + p.basenameWithoutExtension(networkFilePath));
         default:
-          modAqmInjectingStatus.value = appText.fileDownloadFailed;
+          status.value = appText.fileDownloadFailed;
       }
     }
   }
